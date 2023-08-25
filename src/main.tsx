62284b81a4ab6b6ddc33aa45beae687c708ea644
Anomalyforlife <themrsimon1@gmail.com>
2023-08-25 17:29:32 +0200

    feat: :sparkles: Added Routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './Routes.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ChakraProvider>
